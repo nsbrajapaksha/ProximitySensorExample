a28e642e1f3a1775bf7574ded46b942d44f4eeb1
nsbr <nshalitha7@gmail.com>
2017-11-28 18:43:56 +0530

    screen turn on/off

--- app/src/main/java/com/example/nsbr/proximitysensorexample/SensorActivity.java
+++ app/src/main/java/com/example/nsbr/proximitysensorexample/SensorActivity.java
@@ -1,10 +1,12 @@
 package com.example.nsbr.proximitysensorexample;
 
+import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.PowerManager;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
@@ -12,6 +14,8 @@ public class SensorActivity extends AppCompatActivity implements SensorEventList
 
     private SensorManager mSensorManager;
     private Sensor mSensor;
+    private PowerManager mPowerManager;
+    private PowerManager.WakeLock mWakeLock;
     TextView tv;
 
     @Override
@@ -29,9 +33,15 @@ public class SensorActivity extends AppCompatActivity implements SensorEventList
         if (event.values[0] == 0) {
             //near
             tv.setText("near");
+            mPowerManager = (PowerManager) this.getSystemService(Context.POWER_SERVICE);
+            mWakeLock = mPowerManager.newWakeLock(PowerManager.PROXIMITY_SCREEN_OFF_WAKE_LOCK, "tag");
+            mWakeLock.acquire();
         } else {
             //far
             tv.setText("far");
+            mPowerManager = (PowerManager) this.getSystemService(Context.POWER_SERVICE);
+            mWakeLock = mPowerManager.newWakeLock(PowerManager.SCREEN_BRIGHT_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP, "tag");
+            mWakeLock.acquire();
         }
     }
 
